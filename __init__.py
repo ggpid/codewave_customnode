a1ca60581f7caa35a3c42fe41647c856b30933c5
ggpid <ggpid77@gmail.com>
2026-02-24 13:15:57 +0900

    a

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
 from .extract_layer import ExtractTransparentLayer
 from .color_to_transparent import ColorToTransparent
+from .extract_windows import ExtractWindows
 
 NODE_CLASS_MAPPINGS = {
     "ExtractTransparentLayer": ExtractTransparentLayer,
     "ColorToTransparent": ColorToTransparent,
+    "ExtractWindows": ExtractWindows,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ExtractTransparentLayer": "Extract Transparent Layer",
     "ColorToTransparent": "Color To Transparent",
+    "ExtractWindows": "Extract Windows",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
